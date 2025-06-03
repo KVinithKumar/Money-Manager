efb6762ec36d31342ec8949dbde65395507cdbdc
KVinithKumar <162706775+KVinithKumar@users.noreply.github.com>
2025-06-03 23:41:48 +0530

    rcb-wins

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 3001;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://money-manager-qzog.vercel.app/login"],
+    origin: ["http://localhost:3000", "https://money-manager-qzog.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
